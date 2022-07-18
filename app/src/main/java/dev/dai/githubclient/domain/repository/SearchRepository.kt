6f71d02c3ceaf64523e5126a71a70c3ab7f8bf94
Dai1678 <dai7774@gmail.com>
2022-07-18 20:37:37 +0900

    SearchRepositoryのdiができるようにModuleを実装

--- app/src/main/java/dev/dai/githubclient/domain/repository/SearchRepository.kt
+++ app/src/main/java/dev/dai/githubclient/domain/repository/SearchRepository.kt
@@ -3,12 +3,15 @@ package dev.dai.githubclient.domain.repository
 import dev.dai.githubclient.domain.mapper.toUserSearchResultIndex
 import dev.dai.githubclient.domain.model.UserSearchResultIndex
 import dev.dai.githubclient.infra.api.GithubApi
+import javax.inject.Inject
 
 interface SearchRepository {
   suspend fun searchUser(userName: String): UserSearchResultIndex
 }
 
-class DefaultSearchRepository(private val githubApi: GithubApi) : SearchRepository {
+class DefaultSearchRepository @Inject constructor(
+  private val githubApi: GithubApi
+) : SearchRepository {
   override suspend fun searchUser(userName: String): UserSearchResultIndex {
     return githubApi.searchUsers(userName).toUserSearchResultIndex()
   }
