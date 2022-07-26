d88acc099ed4cc7817e1403f41afc6495969105e
Dai1678 <dai7774@gmail.com>
2022-07-26 13:38:48 +0900

    ユーザー詳細の取得処理を実装

--- app/src/main/java/dev/dai/githubclient/model/GithubRepo.kt
+++ app/src/main/java/dev/dai/githubclient/model/GithubRepo.kt
@@ -2,9 +2,9 @@ package dev.dai.githubclient.model
 
 data class GithubRepo(
   val title: String,
-  val description: String,
-  val form: Boolean,
+  val description: String?,
+  val fork: Boolean,
   val url: String,
-  val language: String,
+  val language: String?,
   val stargazerCount: Int
 )
